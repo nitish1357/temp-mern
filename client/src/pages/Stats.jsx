8a9109cf821ee95929f5a92527e524e298530e1b
Nitish1357 <nitishmehta1990@gmail.com>
2024-02-25 00:48:17 +0530

    First query with react query

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,13 +1,24 @@
 import { ChartsContainer, StatsContainer } from '../components';
 import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
 export const loader = async () => {
-  const response = await customFetch.get('/jobs/statss');
+  return null;
+  const response = await customFetch.get('/jobs/stats');
   return response.data;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => customFetch.get('/jobs/stats'),
+  });
+
+  if (isLoading) return <h4>Loading...</h4>;
+  if (isError) return <h4>Error...</h4>;
+  const { defaultStats, monthlyApplications } = data.data;
 
   return (
     <>
